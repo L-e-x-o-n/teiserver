05f725aadc83c6946e84bb2b8d6cad01124a44a6
L-e-x-o-n <44340857+L-e-x-o-n@users.noreply.github.com>
2024-04-12 21:21:58 +0200

    bad commit message

--- config/config.exs
+++ config/config.exs
@@ -162,4 +162,11 @@ config :teiserver, :time_zone_database, Tzdata.TimeZoneDatabase
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
+
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, App.Repo,
+    username: "postgres",
+    password: "postgres"
+else
+  import_config "#{config_env()}.exs"
+end
